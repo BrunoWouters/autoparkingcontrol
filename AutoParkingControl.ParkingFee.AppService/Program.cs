c915c7390e1a066b979cd4d57b19d46abc8e77e6
BrunoWouters <bruno.wouters@gmail.com>
2023-09-01 19:38:19 +0200

    wip

--- AutoParkingControl.ParkingFee.AppService/Program.cs
+++ AutoParkingControl.ParkingFee.AppService/Program.cs
@@ -1,11 +1,13 @@
 using Dapr.AspNetCore;
 using AutoParkingControl.Contracts.Events;
 using Microsoft.AspNetCore.Mvc;
+using Dapr.Client;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDaprClient();
 
 var app = builder.Build();
 
@@ -17,12 +19,19 @@ if (app.Environment.IsDevelopment())
 
 app.UseCloudEvents();
 app.UseRouting();
-app.UseEndpoints(endpoints => endpoints.MapSubscribeHandler());
+app.MapSubscribeHandler();
 
 
-app.MapPost("/sendfine", async ([FromBody] RequiredSessionNotFound requiredSessionNotFound) =>
+app.MapPost("/sendfine", async ([FromBody] RequiredSessionNotFound requiredSessionNotFound, DaprClient daprClient) =>
 {
-
+    var body = 
+    @$"<b>Fine for {requiredSessionNotFound.LicencePlate}</b><br>
+    On {requiredSessionNotFound.VehicleDetectedOn.ToLocalTime():G} we found your vehicle parked without a valid session.";
+    await daprClient.InvokeBindingAsync("smtp", "create", body, new Dictionary<string, string>{
+        {"emailFrom", "finedep@autoparkingcontrol.com"},
+        {"emailTo", "finereceipient@mailprovider.com"},
+        {"subject", "Fine"}
+    });
 }).WithTopic("pubsub", "RequiredSessionNotFound");
 
 app.Run();
