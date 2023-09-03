5c4282049ab8bdd95902525e0aba5623f302cfca
BrunoWouters <bruno.wouters@gmail.com>
2023-09-03 15:02:04 +0200

    wip

--- AutoParkingControl.LicencePlateRecognition.ApiService/Program.cs
+++ AutoParkingControl.LicencePlateRecognition.ApiService/Program.cs
@@ -20,42 +20,107 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.MapPost("/test", async (IFormFile photo, DaprClient daprClient) =>
+{
+    var timestamp = DateTime.UtcNow;
+    var httpClient = new HttpClient(); //TODO: Cache http client
+    ByteArrayContent photoContent;
+    using (var photoStream = new MemoryStream())
+    {
+        await photo.CopyToAsync(photoStream);
+        photoContent = new ByteArrayContent(photoStream.ToArray());
+    }
+
+    var ocrUrl = "https://westeurope.api.cognitive.microsoft.com/computervision/imageanalysis:analyze?features=read&api-version=2023-04-01-preview";
+    var apiKey = (await daprClient.GetSecretAsync("apc-secret-store", "computervision"))["apikey"];
+    httpClient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", apiKey);
+    photoContent.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+    var response = await httpClient.PostAsync(ocrUrl, photoContent);
+    var imageAnalysisResult = await response.Content.ReadFromJsonAsync<ImageAnalysisResult>();
+
+    await ProcesExtractedTextAsync(timestamp, imageAnalysisResult?.ReadResult?.Content);
+});
+
+app.MapPost("/invokehttpendpoint", async (IFormFile photo, DaprClient daprClient) =>
+{
+    var timestamp = DateTime.UtcNow;
+
+    var httpClient = DaprClient.CreateInvokeHttpClient();
+
+
+    ByteArrayContent photoContent;
+    using (var photoStream = new MemoryStream())
+    {
+        await photo.CopyToAsync(photoStream);
+        photoContent = new ByteArrayContent(photoStream.ToArray());
+    }
+
+    var ocrUrl = "http://computervision/computervision/imageanalysis:analyze?features=read&api-version=2023-04-01-preview";
+    photoContent.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+    var response = await httpClient.PostAsync(ocrUrl, photoContent);
+    var imageAnalysisResult = await response.Content.ReadFromJsonAsync<ImageAnalysisResult>();
+
+    await ProcesExtractedTextAsync(timestamp, imageAnalysisResult?.ReadResult?.Content);
+});
+
+app.MapPost("/invokefqdn", async (IFormFile photo, DaprClient daprClient) =>
+{
+    var timestamp = DateTime.UtcNow;
+
+    var httpClient = new HttpClient();
+
+    ByteArrayContent photoContent;
+    using (var photoStream = new MemoryStream())
+    {
+        await photo.CopyToAsync(photoStream);
+        photoContent = new ByteArrayContent(photoStream.ToArray());
+    }
+
+    var ocrUrl = $"http://localhost:{Environment.GetEnvironmentVariable("DAPR_HTTP_PORT")}/v1.0/invoke/https://westeurope.api.cognitive.microsoft.com/method/computervision/imageanalysis:analyze?features=read&api-version=2023-04-01-preview";
+    photoContent.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+    var apiKey = (await daprClient.GetSecretAsync("apc-secret-store", "computervision"))["apikey"];
+    httpClient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", apiKey);
+    var response = await httpClient.PostAsync(ocrUrl, photoContent);
+    var imageAnalysisResult = await response.Content.ReadFromJsonAsync<ImageAnalysisResult>();
+
+    await ProcesExtractedTextAsync(timestamp, imageAnalysisResult?.ReadResult?.Content);
+});
+
 app.MapPost("/upload", async (
     IFormFile photo,
     ILogger<Program> logger) =>
 {
     var timestamp = DateTime.UtcNow;
+    await ProcesExtractedTextAsync(timestamp, "***\n2 - DDJ - 413\nB");
+});
 
-    var computerVisionClient = DaprClient.CreateInvokeHttpClient(appId: "computervision");
-    using (var photoStream = new MemoryStream())
-    {
-        await photo.CopyToAsync(photoStream);
-        using (var photoStreamContent = new StreamContent(photoStream))
-        {
-            var ocrUrl = "computervision/imageanalysis:analyze?features=denseCaptions,read&api-version=2023-04-01-preview";
-            photoStreamContent.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
-            var response = await computerVisionClient.PostAsync(ocrUrl, photoStreamContent);
-            var responseString = await response.Content.ReadAsStringAsync();
-        }
-    }
 
-    //TODO: photo -> OCR -> text
-    var recognisedText = "***\n2 - DDJ - 413\nB";
+app.Run();
 
+
+async Task ProcesExtractedTextAsync(DateTime timestamp, string? extractedText)
+{
+    if(extractedText == null) return;
     var europeseNummerplaatRegex = new Regex(@"(?<indexCijfer>\d)\s*-\s*(?<letters>[A-Z]+)\s*-\s*(?<cijfers>\d\d\d)"); //https://www.vlaanderen.be/de-europese-nummerplaat
-    var matches = europeseNummerplaatRegex.Matches(recognisedText).ToList();
-    if (!matches.Any()) return;
+    var matches = europeseNummerplaatRegex.Matches(extractedText).ToList();
     foreach (Match match in matches)
     {
         if (!match.Success) continue;
         var indexCijfer = match.Groups["indexCijfer"].Value;
         var letters = match.Groups["letters"].Value;
         var cijfers = match.Groups["cijfers"].Value;
-        var licencePlace = $"{indexCijfer}-{letters}-{cijfers}";
-
-        var parkingSessionActor = ActorProxy.Create<IParkingSessionActor>(new Dapr.Actors.ActorId(licencePlace), "ParkingSessionActor");
+        var licencePlate = $"{indexCijfer}-{letters}-{cijfers}";
+        var parkingSessionActor = ActorProxy.Create<IParkingSessionActor>(new Dapr.Actors.ActorId(licencePlate), "ParkingSessionActor");
         await parkingSessionActor.RegisterVehicleDetectionAsync(new RegisterVehicleDetection(timestamp));
     }
-});
+}
 
-app.Run();
+public class ReadResult
+{
+    public string? Content { get; set; }
+}
+
+public class ImageAnalysisResult
+{
+    public ReadResult? ReadResult { get; set; }
+}
